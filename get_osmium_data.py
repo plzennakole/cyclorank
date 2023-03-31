d4802e96c83eaeadc69c4515958df5483481e2e9
Daniel Soutner <dan@soutner.cz>
2023-03-31 19:08:45 +0200

    add applied CZE lanes counting

--- get_osmium_data.py
+++ get_osmium_data.py
@@ -65,7 +65,7 @@ class AmenityListHandler(o.SimpleHandler):
         else:
             return False
 
-    def parse_way_data(self, w):
+    def parse_way_data_original(self, w):
         """Based on https://wiki.openstreetmap.org/wiki/Bicycle"""
         if "highway" in w.tags:
 
@@ -152,6 +152,126 @@ class AmenityListHandler(o.SimpleHandler):
 
                 if self.parse_tag(w, "segregated", ["yes"]):
                     segregated_track_length = cycle_track_length
+
+            if True:
+                if cycle_lane_length + cycle_track_length > 0:
+                    self.way_ids[w.id] = {"raw_distance": cycle_lane_length + cycle_track_length,
+                                          "dist_from_centr": road_distance_from_centroid}
+
+            if self.decay_conf:
+                highway_length = self.apply_weight_decay(highway_length, road_distance_from_centroid)
+                cycle_lane_length = self.apply_weight_decay(cycle_lane_length, road_distance_from_centroid)
+                cycle_track_length = self.apply_weight_decay(cycle_track_length, road_distance_from_centroid)
+                segregated_track_length = self.apply_weight_decay(segregated_track_length, road_distance_from_centroid)
+
+            if True:
+                if cycle_lane_length + cycle_track_length > 0:
+                    self.way_ids[w.id]["weighted_distance"] = cycle_lane_length + cycle_track_length
+
+            self.total_road_length += highway_length
+            self.total_cycle_lane_length += cycle_lane_length
+            self.total_cycle_track_length += cycle_track_length
+            self.total_segregated_cycle_track_length += segregated_track_length
+            self.total_cycling_road_length += cycle_lane_length + cycle_track_length
+
+    def parse_way_data(self, w):
+        """Based on https://wiki.openstreetmap.org/wiki/Bicycle"""
+        if "highway" in w.tags:
+
+            highway_length = o.geom.haversine_distance(w.nodes)
+            road_lats = [n.lat for n in w.nodes]
+            road_lngs = [n.lon for n in w.nodes]
+            road_distances = haversine(road_lats, road_lngs, self.city_centroid.y,
+                                       self.city_centroid.x)
+            road_distance_from_centroid = np.median(road_distances)
+            self.road_distances_from_centroid.append(road_distance_from_centroid)
+
+            cycle_lane_length = 0
+            cycle_track_length = 0
+            segregated_track_length = 0
+
+            # Discount oneways
+            if self.parse_tag(w, "oneway", ["yes"]):
+                highway_length = 0.5 * highway_length
+
+            # Cycle lanes
+            if (
+                    # L1a, L1b, M1, M2a, M2b, M2c, B2
+                    (self.parse_tag(w, "cycleway", ["lane", "opposite_lane", "soft_lane"])) or
+                    # L1a, L1b, L2, M1, M2a, M2d, M3b, S2
+                    (self.parse_tag(w, "cycleway:right", ["lane", "opposite_lane", "soft_lane"])) or
+                    # L1a, L1b, M1, M2b, M2d, M3a
+                    (self.parse_tag(w, "cycleway:left", ["lane", "opposite_lane", "soft_lane"])) or
+                    # L1a
+                    (self.parse_tag(w, "cycleway:both", ["lane", "opposite_lane", "soft_lane"])) or
+                    # B1
+                    ("bicycle:lanes" in w.tags) or
+                    # B3 / other share_busway values
+                    (self.parse_tag(w, "cycleway",
+                                    ["share_busway", "opposite_share_busway", "shoulder", "shared_lane"])) or
+                    (self.parse_tag(w, "cycleway:right",
+                                    ["share_busway", "opposite_share_busway", "shoulder", "shared_lane"])) or
+                    (self.parse_tag(w, "cycleway:left",
+                                    ["share_busway", "opposite_share_busway", "shoulder", "shared_lane"])) or
+                    (self.parse_tag(w, "cycleway:both",
+                                    ["share_busway", "opposite_share_busway", "shoulder", "shared_lane"])) or
+                    # Sidewalks with explicit cycling
+                    (self.parse_tag(w, "sidewalk:both:bicycle", ["designated", "yes"])) or
+                    (self.parse_tag(w, "sidewalk:left:bicycle", ["designated", "yes"])) or
+                    (self.parse_tag(w, "sidewalk:right:bicycle", ["designated", "yes"])) or
+                    (self.parse_tag(w, "sidewalk:bicycle", ["designated", "yes"]))
+            ):
+                # Discount ways that do not have lane going both ways
+                if not (
+                        self.parse_tag(w, "cycleway", ["lane"]) or
+                        self.parse_tag(w, "cycleway:both", ["lane"]) or
+                        (self.parse_tag(w, "cycleway:right", ["lane"]) and self.parse_tag(w, "cycleway:left",
+                                                                                          ["lane"])) or
+                        (self.parse_tag(w, "cycleway:right", ["lane"]) and self.parse_tag(w, "cycleway:right:oneway",
+                                                                                          ["no"])) or
+                        (self.parse_tag(w, "cycleway:left", ["lane"]) and self.parse_tag(w, "cycleway:left:oneway",
+                                                                                         ["no"]))
+                ):
+                    cycle_lane_length = highway_length * 0.5
+                else:
+                    cycle_lane_length = highway_length
+
+                # bus lines are not segregated, weight them 0.2
+                if self.parse_tag(w, "cycleway", ["share_busway", "opposite_share_busway"]):
+                    cycle_lane_length = cycle_lane_length * 0.2
+
+            # Cycle tracks
+            if (
+                    (self.parse_tag(w, "cycleway", ["track", "opposite_track"])) or
+                    (self.parse_tag(w, "cycleway:both", ["track", "opposite_track"])) or
+                    (self.parse_tag(w, "cycleway:left", ["track", "opposite_track"])) or
+                    (self.parse_tag(w, "cycleway:right", ["track", "opposite_track"])) or
+                    (self.parse_tag(w, "highway", ["cycleway"])) or
+                    (self.parse_tag(w, "highway", ["path", "footway"]) and
+                     self.parse_tag(w, "bicycle", ["designated"]) and self.parse_tag(w, "bicycle", ["yes"])) or
+                    (self.parse_tag(w, "cyclestreet", ["yes"])) or
+                    (self.parse_tag(w, "bicycle_road", ["yes"])) or
+                    (self.parse_tag(w, "highway", ["pedastrian"]) and self.parse_tag(w, "bicycle", ["yes"]))
+            ):
+                # Discount oneways
+                if (
+                        (self.parse_tag(w, "highway", ["cycleway"]) and self.parse_tag(w, "oneway", ["yes"])) or
+                        self.parse_tag(w, "oneway:bicycle", ["yes"]) or
+                        self.parse_tag(w, "cycleway:right:oneway", ["yes"]) or
+                        self.parse_tag(w, "cycleway:left:oneway", ["yes"])
+                ):
+                    cycle_track_length = 0.5 * highway_length
+                else:
+                    cycle_track_length = highway_length
+
+                if self.parse_tag(w, "segregated", ["yes"]):
+                    segregated_track_length = cycle_track_length
+
+            # Bicycle dismount
+            if (
+                    self.parse_tag(w, "highway", ["no", "dismount"])
+                ):
+                cycle_track_length = 0  # Dismounting is not cycling :)
 
             if True:
                 if cycle_lane_length + cycle_track_length > 0:
