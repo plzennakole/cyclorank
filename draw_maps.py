11f3d4f930c624136219c880605d05667e8fdea1
Daniel Soutner <dan@soutner.cz>
2023-04-02 21:18:42 +0200

    update outputs

--- draw_maps.py
+++ draw_maps.py
@@ -2,6 +2,8 @@ import pickle
 import sys
 import json
 
+import tqdm
+import matplotlib.pyplot as plt
 from pyrosm import OSM
 
 if __name__ == "__main__":
@@ -15,8 +17,11 @@ if __name__ == "__main__":
         experiment_name = "data/2014-01-01/"
         city_mappings = json.load(open("city_conf_czechia.json"))
 
+    exp_date = experiment_name.split("/")[-2] if experiment_name.endswith("/") else experiment_name.split("/")[-1]
+
     for country_map in city_mappings:
-        for city in city_mappings[country_map]:
+        for city in tqdm.tqdm(city_mappings[country_map]):
+            print(city)
             osm_city = list(city.keys())[0]
 
             filepath = f'{experiment_name}/extracted_maps/{osm_city}.pbf'
@@ -34,8 +39,22 @@ if __name__ == "__main__":
 
             subset.plot(figsize=(35, 35), column="highway", legend=True)
 
-            m = subset.explore()
-            m.save(f"{experiment_name}/{osm_city}.html")
+            try:
+                m = subset.explore()
+                m.save(f"{experiment_name}/{osm_city}.html")
+            except Exception as e:
+                print(e)
 
-            m = lanes.explore()
-            m.save(f"{experiment_name}/{osm_city}_lanes.html")
+            try:
+                m = lanes.explore()
+                m.save(f"{experiment_name}/{osm_city}_lanes.html")
+            except Exception as e:
+                print(e)
+
+            try:  # just becoause of Rumburk with no lanes
+                _ = subset.plot(figsize=(15, 15), column="highway", legend=True, linewidth=5)
+                plt.title(f"{osm_city} - all ways, {exp_date}")
+                plt.savefig(f'{experiment_name}/{osm_city}.png', dpi=300)
+                plt.close()
+            except Exception as e:
+                print(e)
