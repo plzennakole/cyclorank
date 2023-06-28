cbc41621eb5e14d941ac366cdcac4a20f3a51986
Daniel Soutner <dan@soutner.cz>
2023-06-28 11:09:26 +0200

    rmv map

--- draw_maps.py
+++ draw_maps.py
@@ -5,6 +5,7 @@ import json
 import tqdm
 import matplotlib.pyplot as plt
 from pyrosm import OSM
+import networkx as nx
 
 if __name__ == "__main__":
 
@@ -37,8 +38,6 @@ if __name__ == "__main__":
             lanes = subset[subset["highway"] != "cycleway"]
             segregated_lanes = subset[subset["highway"] != "cycleway"]
 
-            subset.plot(figsize=(35, 35), column="highway", legend=True)
-
             try:
                 m = subset.explore()
                 m.save(f"{experiment_name}/{osm_city}.html")
@@ -51,10 +50,29 @@ if __name__ == "__main__":
             except Exception as e:
                 print(e)
 
-            try:  # just becoause of Rumburk with no lanes
+            try:  # just because of Rumburk with no lanes
                 _ = subset.plot(figsize=(15, 15), column="highway", legend=True, linewidth=5)
                 plt.title(f"{osm_city} - all ways, {exp_date}")
                 plt.savefig(f'{experiment_name}/{osm_city}.png', dpi=300)
                 plt.close()
             except Exception as e:
                 print(e)
+
+            continue
+
+            # Convert the network to a NetworkX graph
+            graph = nx.MultiDiGraph()
+            graph.add_edges_from(subset[["u", "v", "key"]].to_records(index=False))
+
+            # Calculate the degree distribution of the graph
+            degree_sequence = sorted([d for n, d in graph.degree()], reverse=True)
+            degree_count = nx.degree_histogram(graph)
+
+            # Plot the degree distribution
+            fig, ax = plt.subplots()
+            ax.plot(degree_sequence, degree_count, "ro-")
+            ax.set_xscale("log")
+            ax.set_yscale("log")
+            ax.set_xlabel("Degree")
+            ax.set_ylabel("Count")
+            plt.show()
