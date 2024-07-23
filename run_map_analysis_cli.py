3acaf0ac9a095e252b327e84f890ca24242c80e4
dan <dan@soutner.cz>
2024-07-23 22:29:02 +0200

    fix

--- run_map_analysis_cli.py
+++ run_map_analysis_cli.py
@@ -11,21 +11,22 @@ logger = logging.getLogger(__name__)
 
 def main(city_mappings: dict, experiment_name: str = "exp"):
     for country_map in city_mappings:
+        logger.info(f"Working on {country_map}")
         for city in city_mappings[country_map]:
             city_name = list(city.keys())[0]
             try:
                 map_path = f"{experiment_name}/extracted_maps/{city_name}.pbf"
                 results_path = f"{experiment_name}/results/{city_name}_decay.json"
                 if os.path.exists(results_path):
-                    print(f"Skipping {city_name}")
+                    logger.info(f"Skipping {city_name}")
                 else:
-                    print(f"Working on {city_name}")
+                    logger.info(f"Working on {city_name}")
                     osm_file = f"{experiment_name}/extracted_maps/{city_name}.pbf"
                     osm_for_one_city(osm_file, city_name, True, experiment_name)
             except KeyboardInterrupt:
-                raise
+                raise KeyboardInterrupt
             except Exception as e:
-                print(e)
+                logger.error(f"Error processing {city_name}: {e}")
                 continue
 
 
@@ -37,9 +38,11 @@ if __name__ == "__main__":
     parser.add_argument("--log_level", type=str, default="INFO")
     args = parser.parse_args()
 
+    logging.basicConfig(level=args.log_level, format="%(asctime)s %(levelname)s %(message)s")
+
     city_mappings = json.load(open(args.config_path))
 
     os.makedirs(f"{args.experiment_name}/extracted_maps", exist_ok=True)
     os.makedirs(f"{args.experiment_name}/results", exist_ok=True)
 
-    main(args.experiment_name, args.config_path)
+    main(city_mappings, args.experiment_name)
